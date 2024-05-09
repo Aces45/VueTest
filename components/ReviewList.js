f4ffe473aca9c7588ae8ba4413787229047fb621
Aces45 <stevenignacio45@yahoo.com>
2024-05-09 11:05:40 +0800

    added a review toggle button

--- components/ReviewList.js
+++ components/ReviewList.js
@@ -9,16 +9,22 @@ app.component('review-list', {
         /*html*/
         `
     <div class="border">
-    <h3>Reviews: ({{ reviews.length }})</h3>    
-        <img v-for="item in reviews.length" :src="creamPuffs[Math.floor(Math.random() * creamPuffs.length)]">
-        <!--<ul>
-            <li v-for="(review, index) in reviews" :key="index" style="list-style-type:none; margin-bottom: 3rem">
-                <div style="height: 30px; margin-bottom: 2rem; text-wrap: wrap">                    
-                    <div style="display: flex; flex-direction: row">{{review.name}} gave this <img v-for="index in 5" src="./images/star.png" style="height:20px; width: 20px; border: 0px; margin: 0 4px" :key="index"> stars!</div>
-                    <p>"{{ review.review }}"</p>                  
-                </div>
-            </li>
-        </ul>-->
+        <button class="button" @click="toggleReviews()" style="margin: 0">
+            <span v-if="showReviews">Hide reviews</span>
+            <span v-else="!showReviews">Show reviews</span>
+        </button>
+        <div v-if="showReviews">
+            <h3>Reviews: ({{ reviews.length }})</h3>
+            <!--<img v-for="item in reviews.length" :src="creamPuffs[Math.floor(Math.random() * creamPuffs.length)]">-->
+            <ul style="padding-left: 0">
+                <li v-for="(review, index) in reviews" :key="index" style="list-style-type:none; margin-bottom: 3rem; white-space:nowrap">
+                    <div style="height: 30px; margin-bottom: 3rem; text-wrap: wrap">
+                        <div style="display: flex; flex-direction: row;">{{review.name}} gave this <img v-for="index in review.rating" src="./images/star.png" style="height:20px; width: 20px; border: 0px; margin: 0 4px" :key="index"><span v-if="review.rating == 1">star!</span> <span v-else>stars!</span></div>
+                    <p style="font-style:italic; font-weight: bold">"{{ review.review }}"</p>
+                    </div>
+                </li>
+            </ul>
+        </div>
     </div>
     `,
     data() {
@@ -30,6 +36,12 @@ app.component('review-list', {
                 './images/creampuff4.png',
                 './images/creampuff5.png'
             ],
+            showReviews: true
         }
-    }
+    },
+    methods: {
+        toggleReviews() {
+            this.showReviews = !this.showReviews;
+        }
+    },
 });
